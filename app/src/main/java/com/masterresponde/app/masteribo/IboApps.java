462e062defecb9f6a5f06ee98829438c88aa9568
luisaguiar659-lang <luis.aguiar659@gmail.com>
2026-09-06 22:27:21 -0300

    Add Master IBO app catalog

---
+++ app/src/main/java/com/masterresponde/app/masteribo/IboApps.java
@@ -0,0 +1,20 @@
+package com.masterresponde.app.masteribo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IboApps {
+
+    private IboApps() {}
+
+    public static final List<String> AVAILABLE_APPS = Arrays.asList(
+            "IBO REVENDA",
+            "IBONEW",
+            "ZONE X",
+            "VU REVENDA",
+            "UNI REVENDA",
+            "GPC PRO",
+            "GERENCIA MAX",
+            "FACILITA"
+    );
+}
